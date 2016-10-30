1516f5bd6c6ee3113ccfabdcab9e0233ce7c1f0d
Elke Brandes <ebrandes@iastate.edu>
2016-10-29 23:04:15 -0500

    added a query that joins county specific cut offs to yields table

--- 09_extract_annual_yields.sql
+++ 09_extract_annual_yields.sql
@@ -2,18 +2,19 @@
 -- using WITH table AS query to build auxiliary table that are only available for the current query
 -- all values are tranformed to metric units and rounded
 -- the resulting table 09_yields_2011_2014 can be exported in psql to a txt file and used in ArcGIS
+-- change yields from bu/ac to kg/ha
 
-
+/*
 DROP TABLE IF EXISTS "09_yields_2011_2014";
 CREATE TABLE "09_yields_2011_2014" AS
 WITH 
 yieldtable11 AS (
 SELECT
-fips,
+fips_crent,
 cluid_mukey as cluid_mukey11,
-clumuacres / 2.471 as clumuha,
+acres / 2.471 as clumuha,
 ccrop as crop11,
-(CASE WHEN ccrop = 'CG' THEN (yield / 39.368 ) * 2.471 ELSE (yield / 36.7437) * 2.471 END) as yield11
+(CASE WHEN ccrop = 'CG' THEN (yield / 39.368 ) * 2471 ELSE (yield / 36.7437) * 2471 END) as yield11
 FROM clumu_cgsb_profit_2011_2014 
 WHERE year = '2011'
 ),
@@ -21,7 +22,7 @@ yieldtable12 AS (
 SELECT
 cluid_mukey as cluid_mukey12,
 ccrop as crop12,
-(CASE WHEN ccrop = 'CG' THEN (yield / 39.368 ) * 2.471 ELSE (yield / 36.7437) * 2.471 END) as yield12
+(CASE WHEN ccrop = 'CG' THEN (yield / 39.368 ) * 2471 ELSE (yield / 36.7437) * 2471 END) as yield12
 FROM clumu_cgsb_profit_2011_2014 
 WHERE year = '2012'
 ),
@@ -29,7 +30,7 @@ yieldtable13 AS (
 SELECT
 cluid_mukey as cluid_mukey13,
 ccrop as crop13,
-(CASE WHEN ccrop = 'CG' THEN (yield / 39.368 ) * 2.471 ELSE (yield / 36.7437) * 2.471 END) as yield13
+(CASE WHEN ccrop = 'CG' THEN (yield / 39.368 ) * 2471 ELSE (yield / 36.7437) * 2471 END) as yield13
 FROM clumu_cgsb_profit_2011_2014 
 WHERE year = '2013'
 ),
@@ -37,7 +38,7 @@ yieldtable14 AS (
 SELECT
 cluid_mukey as cluid_mukey14,
 ccrop as crop14,
-(CASE WHEN ccrop = 'CG' THEN (yield / 39.368 ) * 2.471 ELSE (yield / 36.7437) * 2.471 END) as yield14
+(CASE WHEN ccrop = 'CG' THEN (yield / 39.368 ) * 2471 ELSE (yield / 36.7437) * 2471 END) as yield14
 FROM clumu_cgsb_profit_2011_2014 
 WHERE year = '2014'
 ),
@@ -63,3 +64,20 @@ t2.crop14,
 t2.yield14
 FROM yieldtable1113 as t1
 LEFT JOIN yieldtable14 as t2 ON t1.cluid_mukey11 = t2.cluid_mukey14;
+*/
+-- join yield cutoffs to the yield table 
+
+DROP TABLE IF EXISTS "09_yields_cutoffs_2011_2014";
+CREATE TABLE "09_yields_cutoffs_2011_2014" AS
+SELECT 
+t1.*,
+t2.cut_cg_min_16,
+t2.cut_sb_min_16,
+t2.cut_cg_2nd_16,
+t2.cut_sb_2nd_16,
+t2.cut_cg_min_6,
+t2.cut_sb_min_6,
+t2.cut_cg_2nd_6,
+t2.cut_sb_2nd_6
+FROM "09_yields_2011_2014" AS t1
+LEFT JOIN per_county_min_yields AS t2 ON t1.fips_crent = t2.fips;
