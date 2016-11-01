04c22195171ebd45e6bbec91251b448dc20c77f0
Elke Brandes <ebrandes@iastate.edu>
2016-11-01 15:53:10 -0500

    updated first part of script (N leaching distributions) with new DNDC data (3 swg yield sceanrios)

--- 06_distributions.sql
+++ 06_distributions.sql
@@ -1,42 +1,59 @@
 
 -- distribution of NO3 leaching in the state, on a CLU-mukey level
 
--- the table "05_dndc_clumu_iowa_cgsb_swg_2011_2014" includes clumuacres and NO3 leaching for corn/bean and swg. 
+-- the table "05_dndc_clumu_cgsb_swg" includes clumuha and NO3 leaching for corn/bean and swg. 
 --add two columns with the rounded values 
--- also, convert units to metric system 
-/*
-ALTER TABLE "05_dndc_clumu_iowa_cgsb_swg_2011_2014"
-ADD COLUMN ave_no3_leach_cs_round NUMERIC,
-ADD COLUMN ave_no3_leach_swg_round NUMERIC,
-ADD COLUMN clumuha NUMERIC;
 
-UPDATE "05_dndc_clumu_iowa_cgsb_swg_2011_2014"
-SET ave_no3_leach_cs_round = ROUND(ave_no3_leach_cs * 0.4536 * 2.471,0);
-UPDATE "05_dndc_clumu_iowa_cgsb_swg_2011_2014"
-SET ave_no3_leach_swg_round = ROUND(ave_no3_leach_swg * 0.4536 * 2.471,0);
-UPDATE "05_dndc_clumu_iowa_cgsb_swg_2011_2014"
-SET clumuha = clumuacres / 2.471;
-*/
--- to reduce the number of records, first round NO3 to zero decimals, then aggregate over the same amounts
-/*
+
+ALTER TABLE "05_dndc_clumu_cgsb_swg"
+ADD COLUMN ave_no3_leach_cgsb_round NUMERIC,
+ADD COLUMN ave_no3_leach_swg_7500_round NUMERIC,
+ADD COLUMN ave_no3_leach_swg_10000_round NUMERIC,
+ADD COLUMN ave_no3_leach_swg_12500_round NUMERIC;
+
+UPDATE "05_dndc_clumu_cgsb_swg"
+SET 
+ave_no3_leach_cgsb_round = ROUND(ave_no3_leach_ha_cgsb,0),
+ave_no3_leach_swg_7500_round = ROUND(ave_no3_leach_ha_swg_7500,0),
+ave_no3_leach_swg_10000_round = ROUND(ave_no3_leach_ha_swg_10000,0),
+ave_no3_leach_swg_12500_round = ROUND(ave_no3_leach_ha_swg_12500,0);
+
+-- aggregate over the same leaching values
+
 
 
 DROP TABLE IF EXISTS "06_cgsb_ave_no3_leach_rounded_aggr";
 CREATE TABLE "06_cgsb_ave_no3_leach_rounded_aggr"
 AS SELECT
-ave_no3_leach_cs_round,
+ave_no3_leach_cgsb_round,
 sum(clumuha) AS area_ha
-FROM "05_dndc_clumu_iowa_cgsb_swg_2011_2014"
-GROUP BY ave_no3_leach_cs_round;
+FROM "05_dndc_clumu_cgsb_swg"
+GROUP BY ave_no3_leach_cgsb_round;
 
-DROP TABLE IF EXISTS "06_swg_ave_no3_leach_rounded_aggr";
-CREATE TABLE "06_swg_ave_no3_leach_rounded_aggr"
+
+DROP TABLE IF EXISTS "06_swg_7500_ave_no3_leach_rounded_aggr";
+CREATE TABLE "06_swg_7500_ave_no3_leach_rounded_aggr"
 AS SELECT
-ave_no3_leach_swg_round,
+ave_no3_leach_swg_7500_round,
 sum(clumuha) AS area_ha
-FROM "05_dndc_clumu_iowa_cgsb_swg_2011_2014"
-GROUP BY ave_no3_leach_swg_round;
-*/
+FROM "05_dndc_clumu_cgsb_swg"
+GROUP BY ave_no3_leach_swg_7500_round;
+
+DROP TABLE IF EXISTS "06_swg_10000_ave_no3_leach_rounded_aggr";
+CREATE TABLE "06_swg_10000_ave_no3_leach_rounded_aggr"
+AS SELECT
+ave_no3_leach_swg_10000_round,
+sum(clumuha) AS area_ha
+FROM "05_dndc_clumu_cgsb_swg"
+GROUP BY ave_no3_leach_swg_10000_round;
+
+DROP TABLE IF EXISTS "06_swg_12500_ave_no3_leach_rounded_aggr";
+CREATE TABLE "06_swg_12500_ave_no3_leach_rounded_aggr"
+AS SELECT
+ave_no3_leach_swg_12500_round,
+sum(clumuha) AS area_ha
+FROM "05_dndc_clumu_cgsb_swg"
+GROUP BY ave_no3_leach_swg_12500_round;
 
 -- profit optimized scenario:
 
@@ -80,7 +97,7 @@ GROUP BY ave_no3_leach_swg_round_unprofit;
 
 -- distribution of no3 leaching on CGSB fields of unprofitable areas:
 -- create a new column and filter for those areas
-
+/*
 ALTER TABLE "05_dndc_clumu_iowa_cgsb_swg_2011_2014"
 ADD COLUMN ave_no3_leach_cs_round_highleach NUMERIC;
 UPDATE "05_dndc_clumu_iowa_cgsb_swg_2011_2014"
@@ -95,7 +112,7 @@ ave_no3_leach_cs_round_highleach,
 sum(clumuha) AS area_ha
 FROM "05_dndc_clumu_iowa_cgsb_swg_2011_2014"
 GROUP BY ave_no3_leach_cs_round_highleach;
-
+*/
 -- distribution of no3 leaching on swg fields of unprofitable areas:
 -- create a new column and filter for those areas
 /*
