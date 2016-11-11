c88bb4104e1743f9915093bd8fc6e0b0cc0c0567
IASTATE\ebrandes <ebrandes@iatate.edu>
2016-11-10 21:35:49 -0600

    finished script

--- 12_swg_areas_per_county.sql
+++ 12_swg_areas_per_county.sql
@@ -5,29 +5,76 @@
 -- there are a lot of 'NULL' values for fips, that means that there is no attribute data associated with the polygon 
 -- most likely because that polygon was not continuously in corn/soybean from 2012-2015.
 -- the null values are filled with a character 'NULL'. First I want to replace those by real NULL VALUES.
-
-
-
-DELETE FROM swg_scenarios
-WHERE "FIPS" = 'NULL';
-
+-- Also, there is one row that includes the column NAMES I want to get rid OF.
 /*
-UPDATE swg_scenarios
-SET 
-"IN_SWG_MIN_16_10000_20" = NULL WHERE "IN_SWG_MIN_16_10000_20" = 'NULL',
-"IN_SWG_2ND_16_10000_20" = NULL WHERE "IN_SWG_2ND_16_10000_20" = 'NULL',
-"IN_SWG_MIN_6_10000_20" = NULL WHERE "IN_SWG_MIN_6_10000_20" = 'NULL',
-"IN_SWG_2ND_6_10000_20" = NULL WHERE "IN_SWG_2ND_6_10000_20" = 'NULL';
-
-
-DROP TABLE IF EXISTS "12_swg_areas_min_16_county";
-CREATE TABLE "12_swg_areas_min_16_county"
-AS SELECT
-"FIPS",
-SUM("SHAPE_AREA"::NUMERIC)*0.0001 AS in_swg
-FROM swg_scenarios 
-WHERE "IN_SWG_MIN_16_10000_20" = 'TRUE' AND "FIPS" != 'NULL'
-GROUP BY "FIPS";
-
-SELECT count(*) FROM "12_swg_areas_min_16_county"
+DELETE FROM swg_scenarios
+WHERE "FIPS" = 'NULL' OR "FIPS" = 'FIPS';
 */
+
+
+DROP TABLE IF EXISTS "12_swg_areas_county";
+CREATE TABLE "12_swg_areas_county" AS 
+WITH min_16_table AS (
+SELECT
+"FIPS",
+SUM("SHAPE_AREA"::NUMERIC)*0.0001 AS in_swg_min_16
+FROM swg_scenarios 
+WHERE "IN_SWG_MIN_16_10000_20" = 'TRUE' 
+GROUP BY "FIPS"
+),
+"2nd_16_table" AS (
+SELECT
+"FIPS",
+SUM("SHAPE_AREA"::NUMERIC)*0.0001 AS in_swg_2nd_16
+FROM swg_scenarios 
+WHERE "IN_SWG_2ND_16_10000_20" = 'TRUE' 
+GROUP BY "FIPS"
+),
+min_6_table AS (
+SELECT
+"FIPS",
+SUM("SHAPE_AREA"::NUMERIC)*0.0001 AS in_swg_min_6
+FROM swg_scenarios 
+WHERE "IN_SWG_MIN_6_10000_20" = 'TRUE' 
+GROUP BY "FIPS"
+),
+"2nd_6_table" AS (
+SELECT
+"FIPS",
+SUM("SHAPE_AREA"::NUMERIC)*0.0001 AS in_swg_2nd_6
+FROM swg_scenarios 
+WHERE "IN_SWG_2ND_6_10000_20" = 'TRUE' 
+GROUP BY "FIPS"
+),
+fips_table AS (
+SELECT DISTINCT
+"FIPS"
+FROM swg_scenarios
+),
+fips_min_16_table AS (
+SELECT 
+t1."FIPS",
+t2.in_swg_min_16
+FROM fips_table AS t1
+LEFT JOIN min_16_table AS t2 ON t1."FIPS" = t2."FIPS"
+),
+fips_16_table AS (
+SELECT
+t1.*,
+t2.in_swg_2nd_16
+FROM fips_min_16_table AS t1
+LEFT JOIN "2nd_16_table" AS t2 ON t1."FIPS" = t2."FIPS"
+),
+fips_16_6_table AS (
+SELECT
+t1.*,
+t2.in_swg_min_6
+FROM fips_16_table AS t1
+LEFT JOIN "min_6_table" AS t2 ON t1."FIPS" = t2."FIPS"
+)
+SELECT
+t1.*,
+t2.in_swg_2nd_6
+FROM fips_16_6_table AS t1
+LEFT JOIN "2nd_6_table" AS t2 ON t1."FIPS" = t2."FIPS";
+
